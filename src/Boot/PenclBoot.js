90dbbcdd4945f8e35670931d829411cffa4fe1ed
TerraZero <terrazerocore@gmail.com>
2021-06-23 23:50:59 +0200

    bootable

--- src/Boot/PenclBoot.js
+++ src/Boot/PenclBoot.js
@@ -1,29 +1,51 @@
-const Handler = require('pencl-kit/src/Util/Handler');
-const Reflection = require('pencl-kit/src/Util/Reflection');
+const FileUtil = require('pencl-kit/src/Util/FileUtil');
+const Path = require('path');
+const Handler = require('events');
+const FS = require('fs');
 
 module.exports = class PenclBoot {
 
-  constructor(config = null) {
-    this.config = config || {};
+  constructor(path = null, config = null) {
+    this.path = path;
+    this.root = config && config.root || null;
+    this.file = null;
+    this.config = {};
     this.handler = new Handler();
 
+    if (this.path) {
+      this.file = FileUtil.findFileRoot(this.path, 'pencl.json');
+      if (this.file) {
+        this.root = this.root || Path.dirname(this.file);
+        this.config = require(this.file);
+      }
+    }
+    this.loadConfig(config);
 
     process.on('beforeExit', (...args) => {
       this.triggerSync('exit', ...args);
     });
   }
 
-  addConfig(config) {
+  loadConfig(config) {
     for (const index in config) {
       this.config[index] = config[index];
+    }
+    if (config.includeConfigs && Array.isArray(config.includeConfigs)) {
+      for (const path of config.includeConfigs) {
+        if (FS.existsSync(path)) this.loadConfig(require(path));
+      }
     }
   }
 
   async boot() {
-    for (const plugin of this._loading) {
-      try {
-        require(plugin.name + '/pencl.hook')(this);
-      } catch (e) {}
+    const config = this.getConfig('pencl', {});
+    if (Array.isArray(config.modules)) {
+
+      for (const module of config.modules) {
+        try {
+          require(module + '/pencl.hook')(this);
+        } catch (e) {}
+      }
     }
     await this.trigger('boot', this);
   }
@@ -83,13 +105,13 @@ module.exports = class PenclBoot {
    * @returns {(Object|Array|String|Number)}
    */
   getConfig(name, fallback = null) {
-    return Reflection.getDeep(this.config, name, fallback);
+    return this.config[name] || fallback;
   }
 
   /**
    * @param {import('./PenclPlugin')} plugin 
    */
-  getPluginConfig(plugin) {
+   getPluginConfig(plugin) {
     if (plugin._config_loaded) return plugin.config;
     const config = plugin.config;
     const configs = this.getConfig(plugin.name);
@@ -102,6 +124,17 @@ module.exports = class PenclBoot {
     });
     plugin._config_loaded = true;
     return config;
+  }
+
+  /**
+   * @param {string} path 
+   * @returns {string}
+   */
+  getPath(path) {
+    if (path.startsWith('~')) {
+      return Path.join(this.root, (path.startsWith('~/') ? path.substring(2) : path.substring(1)));
+    }
+    return path;
   }
 
 }
